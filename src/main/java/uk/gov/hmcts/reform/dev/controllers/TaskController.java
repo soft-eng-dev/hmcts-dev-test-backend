83217623695f6d313f35da68990a6e14cb2daf39
Kakusha Venecia <kakushavenecia@gmail.com>
2025-09-12 11:08:29 +0100

    Fix- removed * imports on annotations

--- src/main/java/uk/gov/hmcts/reform/dev/controllers/TaskController.java
+++ src/main/java/uk/gov/hmcts/reform/dev/controllers/TaskController.java
@@ -1,7 +1,15 @@
 package uk.gov.hmcts.reform.dev.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import uk.gov.hmcts.reform.dev.models.Task;
 import uk.gov.hmcts.reform.dev.services.TaskService;
 
